f24dcb83b977bb9c4e6c7f5f13b454b2b50b6c39
Keerthana <darapukeerthana2808@gmail.com>
2025-09-19 16:13:06 +0530

    Full Stack Project

--- COURSEAPI-REACT/src/COMPONENTS/GetCourseById.jsx
+++ COURSEAPI-REACT/src/COMPONENTS/GetCourseById.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import config from "../config";
+import config from "../config.js";
 import "./style.css";
 
 export default function GetCourseById() {
